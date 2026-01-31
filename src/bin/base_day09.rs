3c8f1008430d119c6e8aa23de3ee39df7a47aaf0
Alex Conley <alexanderconley@gmail.com>
2026-01-31 22:55:41 +0000

    Set up day10 problem.
    Update devcontainer settings to disable codex sandbox, since it's already in a sandbox.
    
    Add missing base_day09 file.

---
+++ src/bin/base_day09.rs
@@ -0,0 +1,16 @@
+use rust_advent::Point2d;
+
+fn main() -> std::io::Result<()> {
+    let inputs = rust_advent::read_points2d("09")?;
+    println!("Part 1: {}", part1(&inputs));
+    println!("Part 2: {}", part2(&inputs));
+    Ok(())
+}
+
+fn part1(_inputs: &[Point2d]) -> u64 {
+    todo!("Implement");
+}
+
+fn part2(_inputs: &[Point2d]) -> u64 {
+    todo!("Implement");
+}
