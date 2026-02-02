d55eb1d5a8cef96ca7d314eca5ecabf2d7c3d8d1
Alex Conley <alexanderconley@gmail.com>
2026-02-02 20:55:01 +0000

    Start day 12: prompt and basefile.

---
+++ src/bin/base_day12.rs
@@ -0,0 +1,15 @@
+fn main() -> std::io::Result<()> {
+    let inputs = rust_advent::read_file_as_lines("12")?;
+    println!("Part 1: {}", part1(&inputs));
+    println!("Part 2: {}", part2(&inputs));
+    Ok(())
+}
+
+/// Part 1: Beam splitter
+fn part1(_input: &[String]) -> u32 {
+    todo!("Implement");
+}
+
+fn part2(_input: &[String]) -> u32 {
+    todo!("Implement");
+}
